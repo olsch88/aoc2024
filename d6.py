3fcc294bf6e7cdf0197d964356602d7772d5c254
olsch88 <olsch88@googlemail.com>
2024-12-09 22:40:21 +0100

    day 6, part2, brute force

--- d6.py
+++ d6.py
@@ -1,3 +1,5 @@
+import time
+
 DIRECTION={"up":(-1,0), "down":(1,0), "left":(0,-1), "right":(0,1)}
 
 
@@ -30,22 +32,49 @@ def out_of_bounds(position:tuple[int, int], dimensions:tuple[int, int]):
         return True
     return False
 
-def solve_part1(obstacles: dict, start:tuple[int, int], dimension:tuple[int, int])->int:
+def get_path_lengt(obstacles: dict, start:tuple[int, int], dimension:tuple[int, int])->int:
     current_direction = DIRECTION["up"]
     current_position= start
     visited=set()
+    turning_points=set()
     while True:
-
         visited.add(current_position)
+        if (current_position, current_direction) in turning_points:
+            return -1
         next_step=(current_position[0]+current_direction[0],current_position[1]+current_direction[1] )
         if out_of_bounds(next_step, dimension):
             break
         if obstacles.get(next_step)=="#":
-            current_direction=turn_clockwise(current_direction)
+            turning_points.add((current_position, current_direction))
+            current_direction=turn_clockwise(current_direction)            
             continue
         current_position=next_step
     return len(visited)
 
+
+
+def solve_part1(filename:str)->int:
+    return (get_path_lengt(*read_data(filename)))
+
+def solve_part2(filename:str)->int:
+    obstacles, start, dimension= read_data(filename)
+    number_of_loops=0
+    for row in range(dimension[0]):
+        for col in range(dimension[1]):
+            if (row, col )== start:
+                continue
+            new_obstacls = obstacles.copy()
+            new_obstacls[(row, col)]="#"
+            if get_path_lengt(new_obstacls, start, dimension)==-1:
+                number_of_loops+=1
+    return number_of_loops
+            
 if __name__=="__main__":
-    print(solve_part1(*read_data("d6_input.txt")))
+    start= time.perf_counter()
+    print(solve_part1("d6_input.txt"))
+    print(f"runtime part1: {time.perf_counter()-start}")
+    start= time.perf_counter()
+    print(solve_part2("d6_input.txt"))
+    print(f"runtime part2: {time.perf_counter()-start}")
     
+    # part2: 1720 to high
