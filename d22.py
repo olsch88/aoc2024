37788765419cfb911c4b0229f4302afd42f8591e
olsch88 <olsch88@googlemail.com>
2024-12-23 21:50:32 +0100

    day 22, part1

--- d22.py
+++ d22.py
@@ -4,9 +4,37 @@ def read_numbers(file:str)->list[int]:
         
     return data
 
+
 def process_secret_number(secret: int)-> int:
+    # mix
+    secret=secret ^ (secret*64)
+    # prune
+    secret = secret%16777216
     
+    # mix
+    secret=secret ^ (secret//32)
+    # prune
+    secret = secret % 16777216
+
+    # mix
+    secret=secret ^ (secret*2048)
+    # prune
+    secret = secret%16777216
+    
+    return secret
+
+
+def solve_part1(secrets: list[int])-> int:
+    sum_of_secrets=0
+    for secret in secrets:
+        for _ in range(2000):
+            secret=process_secret_number(secret)
+        sum_of_secrets+=secret
+    return sum_of_secrets
+        
 
 
 if __name__=="__main__":
-    print(read_numbers("d22_sample.txt"))
+    data=read_numbers("d22_input.txt")
+    
+    print(solve_part1(data))
