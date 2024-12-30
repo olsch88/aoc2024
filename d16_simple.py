89ad4c0a73d32db61c7de95e863e005d3723247a
olsch88 <olsch88@googlemail.com>
2024-12-30 22:58:35 +0100

    d16, part1 solved

--- d16_simple.py
+++ d16_simple.py
@@ -39,19 +39,11 @@ def traverse_maze(
     min_cost = 999999
     cost = 0
     queue.append((start_pos, start_dir, cost))
-    priority_queue = []
-    # priority_queue = heapify(priority_queue)
-    heappush(priority_queue, (0, start_pos, start_dir, cost))
-    visited = []
-    # while len(priority_queue) > 0:
+    optimal_cost_per_node = dict()
     while len(queue) > 0:
         # print(len(priority_queue), len(visited))
         # print(len(queue), len(visited))
         current_pos, current_dir, current_cost = queue.popleft()
-        # priority, current_pos, current_dir, current_cost = heappop(priority_queue)
-        current_distance = abs(current_pos[0] - target_pos[0]) + abs(
-            current_pos[1] - target_pos[1]
-        )
 
         if current_pos == target_pos:
             print(current_cost)
@@ -59,10 +51,14 @@ def traverse_maze(
             min_cost = min(min_cost, current_cost)
             continue
         else:
-            visited.append((current_pos, current_dir))
+            if current_cost < optimal_cost_per_node.get(
+                (current_pos, current_dir), 9999999
+            ):
+                optimal_cost_per_node[(current_pos, current_dir)] = current_cost
+            else:
+                continue
             if current_cost > min_cost:
                 continue
-            # visited[current_pos] += 1
 
         for direction in DIRECTIONS:
             if direction == current_dir:
@@ -74,19 +70,10 @@ def traverse_maze(
                 # added_cost = 2001
             else:
                 added_cost = 1001
-            # if (
-            #     current_pos[0] + direction[0],
-            #     current_pos[1] + direction[1],
-            # ) == target_pos:
-            #     min_cost = min(min_cost, current_cost + added_cost)
+
             if (
                 maze[current_pos[0] + direction[0]][current_pos[1] + direction[1]]
                 not in "#S"
-                and (
-                    (current_pos[0] + direction[0], current_pos[1] + direction[1]),
-                    direction,
-                )
-                not in visited
             ):
                 queue.append(
                     (
@@ -95,15 +82,7 @@ def traverse_maze(
                         current_cost + added_cost,
                     )
                 )
-                # heappush(
-                #     priority_queue,
-                #     (
-                #         current_distance,
-                #         (current_pos[0] + direction[0], current_pos[1] + direction[1]),
-                #         direction,
-                #         current_cost + added_cost,
-                #     ),
-                # )
+
     return min_cost
 
 
