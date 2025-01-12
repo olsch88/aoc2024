33abee2584ca79e24c1ac17b6cf270963944e456
olsch88 <olsch88@googlemail.com>
2025-01-12 22:42:30 +0100

    day 20, part 2

--- d20.py
+++ d20.py
@@ -13,6 +13,16 @@ POSSIBLE_TELEPORTS_PART1 = [
     (-1, -1),
 ]
 
+POSSIBLE_TELEPORTS_PART2 = []
+for row in range(-20, 21):
+    for column in range(-20, 21):
+        if abs(row) + abs(column) < 21 and abs(row) + abs(column) > 1:
+            POSSIBLE_TELEPORTS_PART2.append((row, column))
+            # POISSIBLE_TELEPORTS_PART2.append((-row, -column))
+            # POISSIBLE_TELEPORTS_PART2.append((row, -column))
+            # POISSIBLE_TELEPORTS_PART2.append((-row, column))
+# print(POSSIBLE_TELEPORTS_PART2)
+
 
 def read_maze(file: str) -> list[str]:
     with open(file, "r") as f:
@@ -71,9 +81,11 @@ def find_shortcut(maze, distances):
             if maze[next_x][next_y] not in ".E":
                 continue
             if (next_x, next_y) in distances:
-                if (distances[current] - distances[(next_x, next_y)] )-2> 1:
-                    save_counter[distances[current] - distances[(next_x, next_y)]-2] += 1
-                    
+                if (distances[current] - distances[(next_x, next_y)]) - 2 > 1:
+                    save_counter[
+                        distances[current] - distances[(next_x, next_y)] - 2
+                    ] += 1
+
         for neighbor in NEIGHBORS:
             next_x, next_y = current[0] + neighbor[0], current[1] + neighbor[1]
             if maze[next_x][next_y] not in ".E":
@@ -83,17 +95,84 @@ def find_shortcut(maze, distances):
     return save_counter
 
 
+
+
+
+def find_shortcut_v2(maze, distances, range):
+    start = find_start(maze)
+    end = find_end(maze)
+    queue = deque()
+    queue.append(start)
+    visited = set()
+    save_counter = defaultdict(int)
+    start_and_end=set()
+    debug_counter=0
+    while len(queue) > 0:
+        current = queue.popleft()
+        visited.add(current)
+        for target in POSSIBLE_TELEPORTS_PART2:
+            next_x, next_y = current[0] + target[0], current[1] + target[1]
+            distance = abs((abs(next_x) - abs(current[0]))) + abs((abs(next_y) - abs(current[1])))
+            if not (0 <= next_x < len(maze) and 0 <= next_y < len(maze[0])):
+                continue
+            if maze[next_x][next_y] not in ".E":
+                continue
+            if (current, (next_x, next_y)) in start_and_end:
+                continue
+            start_and_end.add((current, (next_x, next_y)))
+            if (next_x, next_y) in distances:
+                # if distances[current] - distances[(next_x, next_y)] - distance== 72:
+                #     # print(abs(next_x) - abs(current[0]))#
+                #     # print(abs(next_y) - abs(current[1]))#
+                #     # print(distance)
+                #     print(f"{current}-> {(next_x, next_y)} Distance without: {distances[current]} distance with: {distances[(next_x, next_y)]+distance}")
+                #     debug_counter+=1
+                if current == start and (next_x, next_y) == (7, 3):
+                    print(distance)
+                    print(
+                        f"Teleporting to {(next_x, next_y)} saves {distances[current] - distances[(next_x, next_y)]-distance} picoseconds"
+                    )
+                if (distances[current] - distances[(next_x, next_y)]) - distance > 1:
+                    save_counter[
+                        distances[current] - distances[(next_x, next_y)] - distance
+                    ] += 1
+
+        for neighbor in NEIGHBORS:
+            next_x, next_y = current[0] + neighbor[0], current[1] + neighbor[1]
+            if maze[next_x][next_y] not in ".E":
+                continue
+            if (next_x, next_y) not in visited:
+                queue.append((next_x, next_y))
+    
+    return save_counter
+
+
 def solve_part1(maze: list[str]) -> int:
     distances = get_distances_to_start(maze)
     # print(distances)
     end = find_end(maze)
     shortcut_counter = find_shortcut(maze, distances)
-    counter =0
+    counter = 0
     for key, value in shortcut_counter.items():
         # print(f"There are {value} cheats, that save {key} picoseconds")
         if key >= 100:
             counter += value
-        
+
+    # print(shortcut_counter)
+    return counter
+
+
+def solve_part2(maze: list[str]) -> int:
+    distances = get_distances_to_start(maze)
+    # print(distances)
+    end = find_end(maze)
+    shortcut_counter = find_shortcut_v2(maze, distances, range=20)
+    counter = 0
+    for key, value in shortcut_counter.items():
+        if key >= 100:
+            counter += value
+            # print(f"There are {value} cheats, that save {key} picoseconds")
+
     # print(shortcut_counter)
     return counter
 
@@ -103,4 +182,7 @@ if __name__ == "__main__":
     maze = read_maze("d20_input.txt")
     print(solve_part1(maze))
     print("Execution time:", time.time() - start)
-    # part 1_ 1310 too low
+    start = time.time()
+    print(solve_part2(maze))
+    print("Execution time:", time.time() - start)
+    # part 2_  2209778 to high
